80224c4a9e1383e9d5467fe0c70ee35ea1fba8d9
lokimasta <filmichalak@gmail.com>
2021-01-01 16:03:11 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,8 +1,12 @@
-node default {
+/* node default {
   #file{'/root/README':
    # ensure => file,
     #content => 'This is my README file',
   #}
+}
+*/
+
+node default{
 }
 node 'master.puppet.vm'{
   include role::master_server
